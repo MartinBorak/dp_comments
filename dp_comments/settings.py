b708a6c2583c1b188d57644baf3362489778617c
Martin Borak <martinborak108@gmail.com>
2016-11-02 18:22:47 +0100

    change static settings

--- dp_comments/settings.py
+++ dp_comments/settings.py
@@ -122,11 +122,7 @@ USE_TZ = True
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, '../video/static'),
-)
+STATIC_URL = '/video/static/'
 
 FIXTURE_DIRS = (
    '/video/fixtures/',
