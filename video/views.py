b3a85358059293001f405c9cbf1b3b92a782ab65
Martin Borak <martinborak108@gmail.com>
2016-11-03 19:46:37 +0100

    remove user model import from views

--- video/views.py
+++ video/views.py
@@ -4,7 +4,7 @@ from django.views.generic import View
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
-from .models import Video, Comment, Worker, User
+from .models import Video, Comment, Worker
 from .forms import RegisterForm, RadioForm
 
 
